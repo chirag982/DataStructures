8e73640409bfb7deaec8de5a111f6b537c67aa18
chirag982 <chirag178g@gmail.com>
2024-12-25 13:52:09 +0530

    Created methods liket get,set,insert,remove,reverse etc

--- linkedlist/Main.java
+++ linkedlist/Main.java
@@ -3,18 +3,32 @@ package linkedlist;
 public class Main {
     public static void main(String[] args) {
         
-        LinkedList myLinkedList = new LinkedList(2);
-        myLinkedList.append(1);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+
+        myLinkedList.reverse();
+
+        // myLinkedList.append(23);
+        // myLinkedList.append(7);
+        // myLinkedList.insert(1, 1);
         
-        System.out.println();
+        // System.out.println();
+        
         // myLinkedList.Prepend(1);
         // System.out.println(myLinkedList.removeFirst().value);
         // System.out.println(myLinkedList.removeFirst().value);
         // System.out.println(myLinkedList.removeFirst());
-
+        // myLinkedList.set(2,4);
         System.out.println();
-        myLinkedList.printList();
+        myLinkedList.printList();        
         
+        // System.out.println();
+        // System.out.println(myLinkedList.get(2).value);
+        
+
+
         // // myLinkedList.printList();
         
         // System.out.println(myLinkedList.removeLast().value);
