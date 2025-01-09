cbc80100848097a3a082ad744c6dc65f15c1655a
chirag982 <chirag178g@gmail.com>
2025-01-09 11:51:18 +0530

    Completed the DoublyLinkedList topic

--- doublylinkedlist/DoublyLinkedList.java
+++ doublylinkedlist/DoublyLinkedList.java
@@ -85,4 +85,89 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public Node removeFirst() {
+        if (length==0) return null;
+        Node temp = head;
+        if(length==1){
+            head=null;
+            tail=null;
+        }
+        else {
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }   
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        Node temp = head;
+        if(index < length/2) {
+            for(int i=0; i<index; i++) {
+                temp = temp.next;
+            }
+        }
+        else {
+            temp = tail;
+            for(int i=length-1; i>index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp!=null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index==0) {
+            prepend(value);
+            return true;
+        }
+        if (index==length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node before = get(index-1);
+        Node after = before.next;
+        
+        newNode.next = after;
+        newNode.prev = before;
+        before.next = newNode;
+        after.prev = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index >= length || index < 0) {
+            return null;
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        if (index == length-1) {
+            return removeLast();
+        }
+        Node temp = get(index);
+        temp.next.prev = temp.prev;
+        temp.prev.next = temp.next;
+        temp.next = null;
+        temp.prev = null;
+        length-- ;
+        return temp;
+    }
+
 }
--- doublylinkedlist/Main.java
+++ doublylinkedlist/Main.java
@@ -25,6 +25,38 @@ public class Main {
         // System.out.println(myDLL.removeLast());
 
 
+        // DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        // myDLL.append(1);
+        // myDLL.append(2);
+        // myDLL.append(3);
+        // System.out.println(" ");
+        // System.out.println(myDLL.get(1).value);
+        // System.out.println(myDLL.get(2).value + "\n");
+        // myDLL.printlist();
+
+
+        // DoublyLinkedList myDLL = new DoublyLinkedList(11);
+        // myDLL.append(3);
+        // myDLL.append(23);
+        // myDLL.append(7);
+        // myDLL.set(1,4);
+        // System.out.println("");
+        // myDLL.printlist();
+
+
+        // DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        // myDLL.append(3);
+        // System.out.println("");
+        // myDLL.insert(1,2);
+        // myDLL.printlist();
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+
+        System.out.println("");
+        myDLL.remove(1);
+        myDLL.printlist();
 
     }    
 }
