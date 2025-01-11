01b0bf3becef6345ee2ec30e4c4c256a11ff4272
chirag982 <chirag178g@gmail.com>
2025-01-11 23:02:11 +0530

    Started learning stack!

---
+++ stack/Main.java
@@ -0,0 +1,17 @@
+package stack;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Stack myStack = new Stack(4);
+
+        System.out.println("");
+        myStack.getTop();
+        myStack.getHeight();
+        myStack.printStack();
+
+
+    }
+
+}
