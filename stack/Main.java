eb2c7e72d5fd846cd376aba17832c21ad7f3f267
chirag982 <chirag178g@gmail.com>
2025-01-11 23:12:46 +0530

    created a push method

--- stack/Main.java
+++ stack/Main.java
@@ -4,12 +4,23 @@ public class Main {
     
     public static void main(String[] args) {
         
-        Stack myStack = new Stack(4);
+        // Stack myStack = new Stack(4);
+        // System.out.println("");
+        // myStack.getTop();
+        // myStack.getHeight();
+        // myStack.printStack();
+
+        Stack myStack = new Stack(2);
         System.out.println("");
         myStack.getTop();
         myStack.getHeight();
         myStack.printStack();
 
+        myStack.push(1);
+        System.out.println("");
+        myStack.getTop();
+        myStack.getHeight();
+        myStack.printStack();
 
     }
 
