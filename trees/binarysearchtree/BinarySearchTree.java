7dee5894531e87f5a1076c31a7f71fdadea98fe0
chirag982 <chirag178g@gmail.com>
2025-02-03 00:00:09 +0530

    Added a method contains after insert in bst

--- trees/binarysearchtree/BinarySearchTree.java
+++ trees/binarysearchtree/BinarySearchTree.java
@@ -39,4 +39,21 @@ public class BinarySearchTree {
         }
     }
     
+    public boolean contains(int value) {
+        Node temp = root;
+        while(temp!=null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            }
+            else if (value > temp.value) {
+                temp = temp.right;
+            }
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
