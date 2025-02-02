ddc86f42be6075d21c53de1b6151554be6699d4f
chirag982 <chirag178g@gmail.com>
2025-02-02 19:47:18 +0530

    Learning about binary search tree

---
+++ trees/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,42 @@
+package trees.binarysearchtree;
+
+public class BinarySearchTree {
+    Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;  
+        }
+    }
+
+    public boolean insert(int value) {
+        Node temp = root;
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            }
+            else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+    
+}
