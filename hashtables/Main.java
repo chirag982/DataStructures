3e671942286a40b3b7c636848d785245e45b61be
chirag982 <chirag178g@gmail.com>
2025-02-08 22:43:26 +0530

    Added a hash method and set method

--- hashtables/Main.java
+++ hashtables/Main.java
@@ -2,8 +2,18 @@ package hashtables;
 
 public class Main {
     public static void main(String[] args) {
+        
+        // HashTable myHashTable = new HashTable();
+        // myHashTable.printTable();
+
         HashTable myHashTable = new HashTable();
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
 
         myHashTable.printTable();
+
     }
 }
