6be7d10f84811730e4fd52a6da17db1a5e4afae9
chirag982 <chirag178g@gmail.com>
2025-02-10 11:34:47 +0530

    Created a method called keys

--- hashtables/Main.java
+++ hashtables/Main.java
@@ -18,12 +18,20 @@ public class Main {
         // myHashTable.printTable();
 
 
+        // HashTable myHashTable = new HashTable();
+        // myHashTable.set("nails", 100);
+        // myHashTable.set("tile", 50);
+        // myHashTable.set("lumber", 80);
+        // // myHashTable.printTable();
+        // System.out.println(myHashTable.get("lumber"));
+        // System.out.println(myHashTable.get("bolts"));
+
         HashTable myHashTable = new HashTable();
+        myHashTable.set("paint", 20);
+        myHashTable.set("bolts", 40);
         myHashTable.set("nails", 100);
         myHashTable.set("tile", 50);
         myHashTable.set("lumber", 80);
-        // myHashTable.printTable();
-        System.out.println(myHashTable.get("lumber"));
-        System.out.println(myHashTable.get("bolts"));
+        System.out.println(myHashTable.keys());
     }
 }
