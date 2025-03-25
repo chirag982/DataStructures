5fdc4b05ab0b49379c90d08d124ff880ca2d532f
chirag982 <chirag178g@gmail.com>
2025-03-25 11:51:16 +0530

    added method remove vertex

--- graphs/main.java
+++ graphs/main.java
@@ -15,14 +15,31 @@ public class main {
         // myGraph.printGraph();
 
 
+        // Graph myGraph = new Graph();
+        // myGraph.addVertex("A");
+        // myGraph.addVertex("B");
+        // myGraph.addVertex("C");
+        // myGraph.addEdge("A", "B");
+        // myGraph.addEdge("A", "C");
+        // myGraph.addEdge("B", "C");
+        // myGraph.removeEdge("A", "B");
+        // myGraph.printGraph();
+
+
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
         myGraph.addVertex("B");
         myGraph.addVertex("C");
+        myGraph.addVertex("D");
+
         myGraph.addEdge("A", "B");
         myGraph.addEdge("A", "C");
-        myGraph.addEdge("B", "C");
-        myGraph.removeEdge("A", "B");
+        myGraph.addEdge("A", "D");
+        myGraph.addEdge("B", "D");
+        myGraph.addEdge("C", "D");
+        
+        myGraph.removeVertex("D");
+
         myGraph.printGraph();
     
     }
