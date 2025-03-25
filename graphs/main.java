87e1cb475e5d88e62c8dc20ad46e62027e9754cd
chirag982 <chirag178g@gmail.com>
2025-03-25 11:33:53 +0530

    Adding method add edge after the add vertex

--- graphs/main.java
+++ graphs/main.java
@@ -3,8 +3,15 @@ package graphs;
 public class main {
     public static void main(String[] args) {
 
+        // Graph myGraph = new Graph();
+        // myGraph.addVertex("A");
+        // myGraph.printGraph();
+
+
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addEdge("A", "B");
         myGraph.printGraph();
     
     }
