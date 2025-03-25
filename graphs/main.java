a9bd23dbad64182f22048fd8ce1302edee8bce80
chirag982 <chirag178g@gmail.com>
2025-03-25 11:42:21 +0530

    added method remove edge

--- graphs/main.java
+++ graphs/main.java
@@ -8,10 +8,21 @@ public class main {
         // myGraph.printGraph();
 
 
+        // Graph myGraph = new Graph();
+        // myGraph.addVertex("A");
+        // myGraph.addVertex("B");
+        // myGraph.addEdge("A", "B");
+        // myGraph.printGraph();
+
+
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
         myGraph.addVertex("B");
+        myGraph.addVertex("C");
         myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+        myGraph.removeEdge("A", "B");
         myGraph.printGraph();
     
     }
